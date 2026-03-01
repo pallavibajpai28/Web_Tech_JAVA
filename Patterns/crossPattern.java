813da739f40fa5345f318c66a7cea2ac92efa7ba
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-03-01 22:56:59 +0530

    Update crossPattern.java

--- Patterns/crossPattern.java
+++ Patterns/crossPattern.java
@@ -8,7 +8,7 @@ class crossPattern {
     public static void main(String[] args) throws Exception {
         int num = 5; // num of the window pattern
         for (int i = 1; i <= num; i++) {
-            for (int j = 1; j <= num; j++) {
+            for (int j = 1; j <= num; j++) {  
                 if (i == j || i + j == num + 1) { // explaination : i=j is for left diagonal and i+j=num+1 is for right diagonal
                     System.out.print("* ");
                 } else {
