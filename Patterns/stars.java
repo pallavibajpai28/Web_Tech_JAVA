a43a47e9a0c4c1bb46ce885c73620a2c25d19bd8
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-03-01 22:57:30 +0530

    Update stars.java

--- Patterns/stars.java
+++ Patterns/stars.java
@@ -8,7 +8,7 @@ class stars {
     public static void main(String[] args) {
         for(int i = 1; i<=5; i++)
         {
-            for(int j = 1; j<=4; j++)
+            for(int j = 1; j<=4; j++) 
             {
                 if( i == 1 || j == 3)
                     System.out.print("* ");
