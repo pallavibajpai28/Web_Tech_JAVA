78968688e6860a8cb2b477ad8ae64d520ffb30ca
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-03-10 21:04:05 +0530

    Update logic2.java

--- logic2.java
+++ logic2.java
@@ -19,7 +19,7 @@ public class logic2 {
         System.out.println("1st num: "+a);
         System.out.println("2nd num: "+b);
 
-        a = a + b; //  a=10, b=20   10+20=30... so a=30
+        a = a + b; // suppose a=10, b=20   10+20=30... so a=30
         b = a - b; //  30-20=10 ...so b=10
         a = a - b; //  30-10 ...so a=20
        
@@ -31,4 +31,5 @@ public class logic2 {
 
     }
 }
+
 
