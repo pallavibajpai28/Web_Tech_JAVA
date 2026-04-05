68c8b4c5fbe5fecac8eaf04afd48fd1c32fb2006
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-04-05 18:09:22 +0530

    Add files via upload

---
+++ basic_prog/AreaOfCircle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+    double radius=10.0;
+    double area = 3.14*radius*radius;
+    System.out.println("Area of Circle is: "+area);
+    }
+}
