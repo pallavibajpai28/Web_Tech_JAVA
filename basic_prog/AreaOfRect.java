65644a72b213d99384daaa897c96b493f0f79013
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-04-05 18:11:56 +0530

    Add files via upload

---
+++ basic_prog/AreaOfRect.java
@@ -0,0 +1,16 @@
+//WAJP to create a method which returns area of a rectangle and accepts lenght and breadth of a rectangle as actual arguments
+import java.util.Scanner;
+public class AreaOfRect {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of rectangle: ");
+        double length = sc.nextDouble();
+        System.out.print("Enter breadth of rectangle: ");
+        double breadth = sc.nextDouble();
+        double area = getArea(length, breadth);
+        System.out.println("Area of rectangle is: " + area);
+    }
+    public static double getArea(double length, double breadth) {
+        return length * breadth;
+    }
+}
