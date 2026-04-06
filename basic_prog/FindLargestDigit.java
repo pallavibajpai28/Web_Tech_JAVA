fea794c3a2b5de723157acd9f9c230fcb299f915
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-04-06 22:20:28 +0530

    Add files via upload

---
+++ basic_prog/FindLargestDigit.java
@@ -0,0 +1,19 @@
+class FindLargestDigit
+{
+    public static void main(String [] args)
+    {
+        int num=1269;
+        num=Math.abs(num); //! Works for negative number also 
+        int largedigit=num%10;
+        while(num>0)
+        {
+            int lastdigit=num%10;
+            if(lastdigit>largedigit)
+            {
+                largedigit=lastdigit;
+            }
+            num/=10;
+        }
+        System.out.println(largedigit);
+    }
+}
