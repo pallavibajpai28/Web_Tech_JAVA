5469bc17df92e7fca4cb520063a54094678b6ce0
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-04-06 22:20:59 +0530

    Add files via upload

---
+++ basic_prog/FindSmallestDigit.java
@@ -0,0 +1,19 @@
+class FindSmallestDigit
+{
+    public static void main(String [] args)
+    {
+        int num=1269;
+        num=Math.abs(num); //! Works for negative number also 
+        int smallerdigit=num%10;
+        while(num>0)
+        {
+            int lastdigit=num%10;
+            if(lastdigit<smallerdigit)
+            {
+                smallerdigit=lastdigit;
+            }
+            num/=10;
+        }
+        System.out.println(smallerdigit);
+    }
+}
