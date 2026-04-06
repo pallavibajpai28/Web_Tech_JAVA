b0c21c179253c1a0651a5f200dad87f12dfa69f1
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-04-06 22:19:55 +0530

    Add files via upload

---
+++ basic_prog/Count.java
@@ -0,0 +1,15 @@
+public class Count
+{
+    public static void main(String [] args)
+    {
+        int count=1234;
+        int num=0;
+        while(num>0)
+        {
+            num=num/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
+//WAJP to find out sum of digit
