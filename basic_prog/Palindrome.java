5dcdf6163b2b33d8e82eaa60050a7e546e944ca6
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-04-09 20:38:04 +0530

    Add files via upload

---
+++ basic_prog/Palindrome.java
@@ -0,0 +1,28 @@
+//WAJP to check the given number is palindrome or not
+import java.util.*;
+public class Palindrome {
+    public static boolean isPalindrome(int n)
+    {
+        int rev=0;
+        int original=n;
+
+        while(n>0)
+        {
+            int digit = n % 10;  //fetch last digit
+            rev = rev *10 + digit; //store last digit in reverse
+            n = n/10; //remove last digit from n
+        }
+
+        if(rev==original)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+        System.out.println(isPalindrome(num));
+    }
+}
