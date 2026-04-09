a077a9831b146969dbefdb412da8646a045f36fb
pallavibajpai28 <165793292+pallavibajpai28@users.noreply.github.com>
2026-04-09 20:41:05 +0530

    Update AreaOfCircle2.java

--- basic_prog/AreaOfCircle2.java
+++ basic_prog/AreaOfCircle2.java
@@ -1,7 +1,7 @@
 //Create a method which returns the area of circle and accepts the radius as actual argument.
 import java.util.*;
 public class AreaOfCircle2 {
-    public static double circleArea(double radius)
+    public static double circleArea(double radius) //creating a method so that we can just call it to calculate area everytime
     {
         return 3.14*radius*radius;
     }
